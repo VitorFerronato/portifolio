e62846caab8365abcde5f1a93e35b1388ce3308d
Vitor_Ferronato <vitorferronato@gmail.com>
2022-02-14 15:25:27 -0300

    Create Portfolio

--- js/main.js
+++ js/main.js
@@ -43,3 +43,18 @@ function toggleSkills(){
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
+
+/* Portfolio swiper */
+
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
